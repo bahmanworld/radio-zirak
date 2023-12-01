b37d3558943e182bf0e0a015bba2fa38b93015e1
Bahman World <5371898+bahmanworld@users.noreply.github.com>
2023-12-01 20:00:42 +0330

    first commit

---
+++ sanity/music.ts
@@ -0,0 +1,14 @@
+
+export default {
+    name: 'music',
+    type: 'document',
+    title: 'Music',
+    fields: [
+      {
+        name: 'title',
+        type: 'string',
+        title: 'Title',
+        default_value: Math.random()
+      }
+    ]
+  }
